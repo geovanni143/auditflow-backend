477231aa176cea84e9bfbd7c5c6daeae0848adb8
Geovanni Hernandez Hernandez <geovanni143h@gmail.com>
2026-05-20 21:33:37 -0600

    chore: initialize spring boot backend

---
+++ src/main/java/com/auditflow/AuditflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.auditflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditflowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditflowBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auditflow/AuditflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auditflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
